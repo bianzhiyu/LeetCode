c0c611d9a23799aef62c566de181f1314009852d
bianzhiyu <bianzhiyu@126.com>
2019-03-23 00:19:52 -0700

    addtional

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ All the source files are contained in src folder.
 
 All others files and folders are auxiliary.
 
-bin: It contains all output .class file.
-
 input: It contains input from files.
 
 output: It contains output from files.
+
+ManCompile.bat can be used to compile all java files in Windows systems. 
+One can slightly modify it to select which files to compile.
+
+bin: This is the output path. It contains all output .class file. 
+And I set git to not track all .class files.
--- src/lc291_300/LC291_300.java
+++ src/lc291_300/LC291_300.java
@@ -1,6 +1,11 @@
 package lc291_300;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 import treeCodec.*;
@@ -16,6 +21,103 @@ class Solution292
 		if (n == 0)
 			return false;
 		return true;
+	}
+}
+
+//295. Find Median from Data Stream
+//Runtime: 527 ms, faster than 5.04% of Java online submissions for Find Median from Data Stream.
+//Memory Usage: 70.3 MB, less than 5.01% of Java online submissions for Find Median from Data Stream.
+class MedianFinder
+{
+	List<Integer> l = new ArrayList<Integer>();
+	boolean sorted = false;
+
+	/** initialize your data structure here. */
+	public MedianFinder()
+	{
+
+	}
+
+	public void addNum(int num)
+	{
+		l.add(num);
+		sorted = false;
+	}
+
+	public double findMedian()
+	{
+		if (!sorted)
+		{
+			Collections.sort(l);
+			sorted = true;
+		}
+		if (l.size() % 2 == 1)
+			return l.get(l.size() / 2);
+		return (0.0 + l.get(l.size() / 2 - 1) + l.get(l.size() / 2)) / 2.0;
+	}
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder(); obj.addNum(num); double param_2 =
+ * obj.findMedian();
+ */
+
+//Refer and rewrite.
+//Runtime: 114 ms, faster than 84.93% of Java online submissions for Find Median from Data Stream.
+//Memory Usage: 67.6 MB, less than 19.64% of Java online submissions for Find Median from Data Stream.
+class MedianFinder_2
+{
+	PriorityQueue<Integer> largerPart,smallerPart;
+	int size=0;
+
+	/** initialize your data structure here. */
+	public MedianFinder_2()
+	{
+		largerPart=new PriorityQueue<Integer>();
+		smallerPart=new PriorityQueue<Integer>(
+				(Comparator<? super Integer>) new Comparator<Integer>()
+				{
+					public int compare(Integer x,Integer y)
+					{
+						return y-x;
+					}
+				});
+	}
+
+	public void addNum(int num)
+	{
+		if (size==0)
+		{
+			smallerPart.offer(num);
+		}
+		else if (smallerPart.size()>largerPart.size())
+		{
+			if (num>=smallerPart.peek())
+				largerPart.offer(num);
+			else
+			{
+				largerPart.offer(smallerPart.poll());
+				smallerPart.offer(num);
+			}
+		}
+		else 
+		{
+			if (num<=largerPart.peek())
+				smallerPart.add(num);
+			else 
+			{
+				smallerPart.offer(largerPart.poll());
+				largerPart.offer(num);
+			}
+		}
+		size++;
+	}
+
+	public double findMedian()
+	{
+		if (size%2==0) return (smallerPart.peek()+largerPart.peek())/2.0;
+		return smallerPart.peek();
 	}
 }
 
@@ -87,7 +189,6 @@ class Codec
 	}
 }
 
-
 class Codec_2 extends TreeCodec
 {
 
@@ -100,27 +201,27 @@ class Solution299
 {
 	public String getHint(String secret, String guess)
 	{
-		int len=secret.length();
-		boolean[] bulls=new boolean[len];
-		int bullsnum=0;
-		for (int i=0;i<len;i++)
-			if (secret.charAt(i)==guess.charAt(i))
+		int len = secret.length();
+		boolean[] bulls = new boolean[len];
+		int bullsnum = 0;
+		for (int i = 0; i < len; i++)
+			if (secret.charAt(i) == guess.charAt(i))
 			{
 				bullsnum++;
-				bulls[i]=true;
-			}
-			else bulls[i]=false;
-		int[] secretcount=new int[10],guesscount=new int[10];
-		for (int i=0;i<len;i++)
+				bulls[i] = true;
+			} else
+				bulls[i] = false;
+		int[] secretcount = new int[10], guesscount = new int[10];
+		for (int i = 0; i < len; i++)
 			if (!bulls[i])
 			{
-				secretcount[secret.charAt(i)-'0']++;
-				guesscount[guess.charAt(i)-'0']++;
+				secretcount[secret.charAt(i) - '0']++;
+				guesscount[guess.charAt(i) - '0']++;
 			}
-		int cowsnum=0;
-		for (int i=0;i<10;i++)
-			cowsnum+=Math.min(secretcount[i], guesscount[i]);
-		return bullsnum+"A"+cowsnum+"B";
+		int cowsnum = 0;
+		for (int i = 0; i < 10; i++)
+			cowsnum += Math.min(secretcount[i], guesscount[i]);
+		return bullsnum + "A" + cowsnum + "B";
 	}
 }
 
@@ -149,23 +250,15 @@ class Solution300
 
 public class LC291_300
 {
+	public static void test295()
+	{
+		MedianFinder_2 f=new MedianFinder_2();
+		f.addNum(1);
+		f.addNum(2);
+	}
 	public static void main(String[] args)
 	{
-		Queue<TreeNode> q = new LinkedList<TreeNode>();
-		q.add(null);
-		System.out.println(q.isEmpty());
-
-		TreeNode rt = new TreeNode(1);
-		rt.left = new TreeNode(2);
-		rt.left.right = new TreeNode(3);
-
-		Codec x = new Codec();
-
-		System.out.println(x.serialize(rt));
-
-		TreeNode n = x.deserialize(x.serialize(rt));
-
-		System.out.println(x.serialize(n));
+		test295();
 
 	}
 
