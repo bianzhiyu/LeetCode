a0536f762007d4a1591de86e2e489fa03f325a97
bianzhiyu <bianzhiyu@126.com>
2019-03-26 15:23:28 -0700

    Update LC551_560.java

--- src/lc551_560/LC551_560.java
+++ src/lc551_560/LC551_560.java
@@ -54,55 +54,6 @@ class Solution554
 	}
 }
 
-//560. Subarray Sum Equals K
-//Runtime: 20 ms, faster than 57.87% of Java online submissions for Subarray Sum Equals K.
-//Memory Usage: 42.1 MB, less than 5.12% of Java online submissions for Subarray Sum Equals K.
-class Solution560
-{
-	public int subarraySum(int[] nums, int k)
-	{
-		HashMap<Integer, List<Integer>> hm = new HashMap<Integer, List<Integer>>();
-		int len = nums.length;
-		int[] s = new int[len + 1];
-		s[0] = 0;
-		for (int i = 1; i <= len; i++)
-		{
-			s[i] = s[i - 1] + nums[i - 1];
-			if (!hm.containsKey(s[i]))
-				hm.put(s[i], new ArrayList<Integer>());
-			hm.get(s[i]).add(i);
-		}
-		int tot = 0;
-		for (int i = 1; i <= len; i++)
-		{
-			if (!hm.containsKey(s[i] + k - nums[i - 1]))
-				continue;
-			List<Integer> l = hm.get(s[i] + k - nums[i - 1]);
-			if (l.get(l.size() - 1) < i)
-				continue;
-			int f = 0, r = l.size() - 1;
-			while (f < r)
-			{
-				if (r == f + 1)
-				{
-					if (l.get(f) >= i)
-						r = f;
-					else
-						f = r;
-					break;
-				}
-				int m = (f + r) / 2;
-				if (l.get(m) >= i)
-					r = m;
-				else
-					f = m + 1;
-			}
-			tot += l.size() - r;
-		}
-		return tot;
-	}
-}
-
 //556. Next Greater Element III
 //https://leetcode.com/problems/next-greater-element-iii/discuss/258183/Java-Simple-Code%3A-1ms-beats-100
 //Runtime: 0 ms, faster than 100.00% of Java online submissions for Next Greater Element III.
@@ -203,6 +154,55 @@ class Solution556_2
 		if (sum > (long) Integer.MAX_VALUE)
 			return -1;
 		return (int) sum;
+	}
+}
+
+//560. Subarray Sum Equals K
+//Runtime: 20 ms, faster than 57.87% of Java online submissions for Subarray Sum Equals K.
+//Memory Usage: 42.1 MB, less than 5.12% of Java online submissions for Subarray Sum Equals K.
+class Solution560
+{
+	public int subarraySum(int[] nums, int k)
+	{
+		HashMap<Integer, List<Integer>> hm = new HashMap<Integer, List<Integer>>();
+		int len = nums.length;
+		int[] s = new int[len + 1];
+		s[0] = 0;
+		for (int i = 1; i <= len; i++)
+		{
+			s[i] = s[i - 1] + nums[i - 1];
+			if (!hm.containsKey(s[i]))
+				hm.put(s[i], new ArrayList<Integer>());
+			hm.get(s[i]).add(i);
+		}
+		int tot = 0;
+		for (int i = 1; i <= len; i++)
+		{
+			if (!hm.containsKey(s[i] + k - nums[i - 1]))
+				continue;
+			List<Integer> l = hm.get(s[i] + k - nums[i - 1]);
+			if (l.get(l.size() - 1) < i)
+				continue;
+			int f = 0, r = l.size() - 1;
+			while (f < r)
+			{
+				if (r == f + 1)
+				{
+					if (l.get(f) >= i)
+						r = f;
+					else
+						f = r;
+					break;
+				}
+				int m = (f + r) / 2;
+				if (l.get(m) >= i)
+					r = m;
+				else
+					f = m + 1;
+			}
+			tot += l.size() - r;
+		}
+		return tot;
 	}
 }
 
