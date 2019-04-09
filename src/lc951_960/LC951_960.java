4baf23d8b0c586b574fa894987fc362d208b2082
bianzhiyu <bianzhiyu@126.com>
2019-04-09 15:52:24 -0700

    em

--- src/lc951_960/LC951_960.java
+++ src/lc951_960/LC951_960.java
@@ -9,9 +9,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
 import bbst.BBST;
 import treeCodec.TreeNode;
@@ -90,6 +92,164 @@ class Solution955
 			if (del[j])
 				ct++;
 		return ct;
+	}
+}
+
+//956. Tallest Billboard
+//49 / 74 test cases passed.
+//TLE
+//[227,259,250,262,217,280,243,228,244,269,253,228,262,273,240,253,270,242]
+class Solution956
+{
+	public int tallestBillboard(int[] rods)
+	{
+		int s = 0, len = rods.length;
+		for (int i : rods)
+			s += i;
+		s /= 2;
+		boolean d[][] = new boolean[s + 1][s + 1];
+		d[0][0] = true;
+		for (int i = 0; i < len; i++)
+			for (int j = s; j >= 0; j--)
+				for (int k = s; k >= 0; k--)
+				{
+					d[j][k] = d[j][k] || (j >= rods[i] ? d[j - rods[i]][k] : false)
+							|| (k >= rods[i] ? d[j][k - rods[i]] : false);
+				}
+		int max = 0;
+		for (int j = s; j >= 0; j--)
+			if (d[j][j])
+			{
+				max = j;
+				break;
+			}
+		return max;
+	}
+}
+
+//28 / 74 test cases passed.
+//[192,179,193,201,201,198,221,198,205,220,192,205,196,199]
+class Solution956_2
+{
+	public int tallestBillboard(int[] rods)
+	{
+		int s = 0, len = rods.length;
+		for (int i : rods)
+			s += i;
+		s /= 2;
+		LinkedList<int[]> q = new LinkedList<int[]>();
+		Set<Integer> used = new HashSet<Integer>();
+		q.add(new int[]
+		{ 0, 0 });
+		used.add(0);
+		int max = 0;
+		for (int i = 0; i < len; i++)
+		{
+			LinkedList<int[]> tmp = new LinkedList<int[]>();
+			for (int[] se : q)
+			{
+				int n1, n2, hc;
+				n1 = se[0] + rods[i];
+				n2 = se[1];
+				hc = n1 * 2501 + n2;
+				if (n1 <= s && n2 <= s && !used.contains(hc))
+				{
+					tmp.add(new int[]
+					{ n1, n2 });
+					used.add(hc);
+				}
+				if (n1 == n2 && n1 > max)
+					max = n1;
+				n1 = se[0];
+				n2 = se[1] + rods[i];
+				hc = n1 * 2501 + n2;
+				if (n1 <= s && n2 <= s && !used.contains(hc))
+				{
+					tmp.add(new int[]
+					{ n1, n2 });
+					used.add(hc);
+				}
+				if (n1 == n2 && n1 > max)
+					max = n1;
+			}
+			q.addAll(tmp);
+		}
+		return max;
+	}
+}
+
+//Runtime: 36 ms, faster than 57.69% of Java online submissions for Tallest Billboard.
+//Memory Usage: 40.7 MB, less than 41.18% of Java online submissions for Tallest Billboard.
+
+//std, by awice, solution page.
+class Solution956_3
+{
+	int NINF = Integer.MIN_VALUE / 3;
+	Integer[][] memo;
+
+	public int tallestBillboard(int[] rods)
+	{
+		int N = rods.length;
+		// "memo[n][x]" will be stored at memo[n][5000+x]
+		// Using Integer for default value null
+		memo = new Integer[N][10001];
+		return (int) dp(rods, 0, 5000);
+	}
+
+	public int dp(int[] rods, int i, int s)
+	{
+		if (i == rods.length)
+		{
+			return s == 5000 ? 0 : NINF;
+		} else if (memo[i][s] != null)
+		{
+			return memo[i][s];
+		} else
+		{
+			int ans = dp(rods, i + 1, s);
+			ans = Math.max(ans, dp(rods, i + 1, s - rods[i]));
+			ans = Math.max(ans, rods[i] + dp(rods, i + 1, s + rods[i]));
+			memo[i][s] = ans;
+			return ans;
+		}
+	}
+}
+
+//Runtime: 30 ms, faster than 62.69% of Java online submissions for Tallest Billboard.
+//Memory Usage: 38.8 MB, less than 52.94% of Java online submissions for Tallest Billboard.
+//learn from awice
+
+// For comparison of Solution_1,_2,_3, see
+//https://github.com/bianzhiyu/LeetCode/wiki/951_960
+class Solution956_4
+{
+	private int[][] rec;
+	private boolean[][] checked;
+	private final static int ZEROPOINT = 5005;
+
+	private int dfs(int[] r, int st, int sum)
+	{
+		if (st == r.length)
+			return sum == 0 ? 0 : -1;
+		if (checked[st][ZEROPOINT + sum])
+			return rec[st][ZEROPOINT + sum];
+		int a = dfs(r, st + 1, sum), t;
+		t = dfs(r, st + 1, sum + r[st]);
+		if (t != -1 && t > a)
+			a = t;
+		t = dfs(r, st + 1, sum - r[st]);
+		if (t != -1 && t + r[st] > a)
+			a = t + r[st];
+		checked[st][ZEROPOINT + sum] = true;
+		rec[st][ZEROPOINT + sum] = a;
+		return a;
+	}
+
+	public int tallestBillboard(int[] rods)
+	{
+		rec = new int[rods.length][10010];
+		checked = new boolean[rods.length][10010];
+		return dfs(rods, 0, 0);
 	}
 }
 
@@ -194,7 +354,7 @@ class Solution958
 //Memory Usage: 38.6 MB, less than 29.03% of Java online submissions for Regions Cut By Slashes.
 class Solution959
 {
-	private int N,ct;
+	private int N, ct;
 	private List<List<Integer>> graph;
 
 	private int hashInd(int r, int c, int k)
@@ -232,13 +392,11 @@ class Solution959
 					addLink(i, j, 0, i, j, 2);
 					addLink(i, j, 1, i, j, 3);
 					addLink(i, j, 2, i, j, 3);
-				}
-				else if (g[i].charAt(j)=='/')
+				} else if (g[i].charAt(j) == '/')
 				{
 					addLink(i, j, 0, i, j, 1);
 					addLink(i, j, 2, i, j, 3);
-				}
-				else // '\'
+				} else // '\'
 				{
 					addLink(i, j, 0, i, j, 3);
 					addLink(i, j, 2, i, j, 1);
@@ -246,32 +404,33 @@ class Solution959
 			}
 		}
 	}
-	
+
 	private void color()
 	{
-		ct=0;
-		int M=N*N*4;
-		int[] clr=new int[M];
+		ct = 0;
+		int M = N * N * 4;
+		int[] clr = new int[M];
 		Arrays.fill(clr, -1);
-		for (int i=0;i<M;i++)
-			if (clr[i]==-1)
+		for (int i = 0; i < M; i++)
+			if (clr[i] == -1)
 			{
-				clr[i]=ct;
-				Queue<Integer> q=new LinkedList<Integer>();
+				clr[i] = ct;
+				Queue<Integer> q = new LinkedList<Integer>();
 				q.add(i);
 				while (!q.isEmpty())
 				{
-					int h=q.remove();
-					for (int next:graph.get(h))
-						if (clr[next]==-1)
+					int h = q.remove();
+					for (int next : graph.get(h))
+						if (clr[next] == -1)
 						{
-							clr[next]=ct;
+							clr[next] = ct;
 							q.add(next);
 						}
 				}
 				ct++;
 			}
 	}
+
 	public int regionsBySlashes(String[] grid)
 	{
 		establishGraph(grid);
@@ -315,8 +474,43 @@ public class LC951_960
 		}
 	}
 
+	public static void test956()
+	{
+		try
+		{
+			File inFile = new File("input" + File.separator + "input956.txt");
+			BufferedReader bfr = new BufferedReader(new FileReader(inFile));
+
+			File outFile = new File("output" + File.separator + "output956.txt");
+			BufferedWriter bfw = new BufferedWriter(new FileWriter(outFile));
+
+			String inLine;
+			while ((inLine = bfr.readLine()) != null && inLine.length() > 0)
+			{
+				int[] nums = test.Test.parseIntArr(inLine);
+
+				Solution956_3 s = new Solution956_3();
+
+				System.out.println(new Solution956_4().tallestBillboard(nums));
+
+				int ans = s.tallestBillboard(nums);
+
+				System.out.println("std ans=" + ans);
+				bfw.write("" + ans);
+				bfw.newLine();
+			}
+
+			bfr.close();
+			bfw.flush();
+			bfw.close();
+		} catch (IOException e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+
 	public static void main(String[] args)
 	{
-		test955();
+		test956();
 	}
 }
